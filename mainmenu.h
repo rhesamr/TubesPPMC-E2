7f70260c23d2cb362029ab1d5c0af3a79e864646
rhesamr <38358858+rhesamr@users.noreply.github.com>
2020-04-17 20:52:38 +0700

    Update mainmenu.h

--- mainmenu.h
+++ mainmenu.h
@@ -1,3 +1,16 @@
+/*/*  EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+ *  Modul               : Tugas Besar (9)
+ *  Kelompok		: E-2
+ *  Hari dan Tanggal    : Jumat, 17 April 2020
+ *  Nama (NIM)          : Rhesa Muhammad Ramadhan (13217003);
+ *                        Saif Sulthan Shaleh (18318032);
+ *                        Faatihah Maurisna Ashfahani (18318036);
+ *                        Jonathan Salomo (18318045)
+ *  Asisten (NIM)       : Gabriella Hayley Tanudjaja (13216050)
+ *  Nama File           : mainmenu.h
+ *  Deskripsi           : File header untuk mainmenu.c
+ */
+
 #include <stdio.h>
 
 int main_menu();
